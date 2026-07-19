ab50fd130849bd7f6312ffe2d3d8edf9101d0948
himaaneesh11 <himaaneeshreddy110605@gmail.com>
2026-07-19 15:55:37 +0530

    Uploading Insertaftervalue for circularLL

--- src/LinkedList/CircularLL.java
+++ src/LinkedList/CircularLL.java
@@ -2,6 +2,7 @@ package LinkedList;
 
 public class CircularLL {
     Node head,tail;
+    int size;
     public void Insertaatabeggining(int data){
         Node newnode=new Node(data);
         if(head==null){
@@ -14,11 +15,49 @@ public class CircularLL {
         }
         temp.next=newnode;
         head=newnode;
+        size++;
         /*
         * if both head and tail are present
         * \tail.next=newnode;
         * newnode.nett=head;
         * head=newnode;*/
+    }
+    public void Insertionatend(int data){
+        Node newnode=new Node(data);
+        if (head==null){
+            Insertaatabeggining(data);
+            return;
+        }
+        tail.next=newnode;
+        newnode.next=head;
+        head=newnode;
+        size++;
+    }
+    public void Insertaftervalue(int value,int data){
+        Node newnode=new Node(data);
+        if (head==null){
+            Insertaatabeggining(data);
+            return;
+        }
+        if(head.data==value){
+            newnode.next=head.next;
+            head.next=newnode;
+            size++;
+        }
+        Node temp=head;
+        do{
+            if(temp.data==value){
+                break;
+            }
+            temp=temp.next;
+
+        }while(temp!=head);
+        if(temp==head){
+            System.out.println("No such value exist");
+        }
+        newnode.next=temp.next;
+        temp.next=newnode;
+        size++;
     }
     public static void main(String[] args) {
 
